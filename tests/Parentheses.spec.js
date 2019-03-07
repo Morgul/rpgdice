290cb7d0c63d36745b61e64681ad90cd3801466d
MrFigg <36649520+mrfigg@users.noreply.github.com>
2019-03-07 10:39:11 -0800

    Create Parentheses.spec.js

---
+++ tests/Parentheses.spec.js
@@ -0,0 +1,46 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Unit Tests for the Repeat class.
+// ---------------------------------------------------------------------------------------------------------------------
+
+const { expect } = require('chai');
+
+const parser = require('../lib/parser');
+const Parentheses = require('../lib/Parentheses');
+const Num = require('../lib/Number');
+const Roll = require('../lib/Roll');
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+describe('Parentheses Class', () =>
+{
+    let parentheses;
+    beforeEach(() =>
+    {
+        parentheses = new Parentheses(new Roll(new Num(3), new Num(6)));
+    });
+
+    it('can be converted to json', () =>
+    {
+        expect(JSON.stringify(parentheses)).to.equal('{"type":"parentheses","content":{"type":"roll","count":{"type":"number","value":3},"sides":{"type":"number","value":6},"results":[]}}');
+    });
+
+    it('can be converted to a parsable string', () =>
+    {
+        expect(parentheses.toString()).to.equal('(3d6)');
+        expect(parser.parse(parentheses.toString())).to.deep.equal(parentheses);
+    });
+
+    describe('#eval()', () =>
+    {
+        it('returns itself with the value populated', () =>
+        {
+            parentheses = new Parentheses(new Num(5));
+            const results = parentheses.eval();
+
+            expect(results.value).to.exist;
+            expect(results.value).to.equal(5);
+        });
+    });
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
